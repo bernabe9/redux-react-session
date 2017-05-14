7b15990c55b365afaf5fe98942a1936839f05c7a
Bernabe Gonzalez <bernabe@toptierlabs.com>
2017-05-14 19:11:00 -0300

    add support for server rendering

--- src/index.js
+++ src/index.js
@@ -13,12 +13,11 @@ let instance;
 
 export class sessionService {
   constructor(store, refreshOnCheckAuth, redirectPath) {
-    if (!instance) {
-      instance = this;
-      instance.store = store;
-      instance.refreshOnCheckAuth = refreshOnCheckAuth;
-      instance.redirectPath = redirectPath;
-    }
+    instance = this;
+    instance.store = store;
+    instance.refreshOnCheckAuth = refreshOnCheckAuth;
+    instance.redirectPath = redirectPath;
+    instance.client = false;
     return instance;
   }
 
@@ -31,6 +30,36 @@ export class sessionService {
   static initSessionService(store, refreshOnCheckAuth = false, redirectPath = 'login') {
     instance = new sessionService(store, refreshOnCheckAuth, redirectPath);
     sessionService.refreshFromLocalStorage();
+  }
+
+  static initClientSession(store, refreshOnCheckAuth = false, redirectPath = 'login') {
+    instance = new sessionService(store, refreshOnCheckAuth, redirectPath);
+    instance.client = true;
+    sessionService.refreshFromLocalStorage();
+  }
+
+  static initServerSession(store, req, refreshOnCheckAuth = false, redirectPath = 'login') {
+    instance = new sessionService(store, refreshOnCheckAuth, redirectPath);
+    instance.client = true;
+    const parseCookies = (req) => {
+      const list = {};
+      const rc = req.get('cookie');
+
+      rc && rc.split(';').forEach(cookie => {
+        const parts = cookie.split('=');
+        list[parts[0].trim()] = JSON.parse(decodeURIComponent(parts[1]));
+      });
+
+      return list;
+    };
+    sessionService.refreshFromClient(parseCookies(req));
+  }
+
+  static refreshFromClient(cookies) {
+    if (Object.keys(cookies).length > 0) {
+      sessionService.saveSession(cookies[constant.USER_SESSION]);
+      sessionService.saveUser(cookies[constant.USER_DATA]);
+    }
   }
 
   static refreshFromLocalStorage() {
@@ -67,33 +96,58 @@ export class sessionService {
     });
   }
 
+  static checkAuthServer(nextState, replace, next) {
+    const { store } = instance;
+    const { authenticated } = store.getState().session;
+    if (authenticated) {
+      next();
+    } else {
+      replace({
+        pathname: instance.redirectPath,
+        state: { nextPathname: nextState.location.pathname }
+      });
+      next();
+    }
+  }
+
   static saveSession(session) {
     return new Promise((resolve) => {
-      localForage.setItem(constant.USER_SESSION, session)
-      .then(() => {
-        instance.store.dispatch(getSessionSuccess());
-        resolve();
-      })
-      .catch(() => {
+      if (instance.client) {
         Cookies.set(constant.USER_SESSION, session);
         instance.store.dispatch(getSessionSuccess());
         resolve();
-      });
+      } else {
+        localForage.setItem(constant.USER_SESSION, session)
+        .then(() => {
+          instance.store.dispatch(getSessionSuccess());
+          resolve();
+        })
+        .catch(() => {
+          Cookies.set(constant.USER_SESSION, session);
+          instance.store.dispatch(getSessionSuccess());
+          resolve();
+        });
+      }
     });
   }
 
   static loadSession() {
     return new Promise((resolve, reject) => {
-      localForage.getItem(constant.USER_SESSION)
-      .then((currentSession) => {
-        if (currentSession) {
-          resolve(currentSession);
-        } else {
-          const cookies = Cookies.getJSON(constant.USER_SESSION);
-          cookies ? resolve(cookies) : reject('Session not found');
-        }
-      })
-      .catch(err => reject(err));
+      if (instance.client) {
+        const cookies = Cookies.getJSON(constant.USER_SESSION);
+        cookies ? resolve(cookies) : reject('Session not found');
+      } else {
+        localForage.getItem(constant.USER_SESSION)
+        .then((currentSession) => {
+          if (currentSession) {
+            resolve(currentSession);
+          } else {
+            const cookies = Cookies.getJSON(constant.USER_SESSION);
+            cookies ? resolve(cookies) : reject('Session not found');
+          }
+        })
+        .catch(err => reject(err));
+      }
     });
   }
 
@@ -106,31 +160,42 @@ export class sessionService {
 
   static saveUser(user) {
     return new Promise((resolve) => {
-      localForage.setItem(constant.USER_DATA, user)
-      .then((user) => {
-        instance.store.dispatch(getUserSessionSuccess(user));
-        resolve();
-      })
-      .catch(() => {
+      if (instance.client) {
         instance.store.dispatch(getUserSessionSuccess(user));
         Cookies.set(constant.USER_DATA, user);
         resolve();
-      });
+      } else {
+        localForage.setItem(constant.USER_DATA, user)
+        .then((user) => {
+          instance.store.dispatch(getUserSessionSuccess(user));
+          resolve();
+        })
+        .catch(() => {
+          instance.store.dispatch(getUserSessionSuccess(user));
+          Cookies.set(constant.USER_DATA, user);
+          resolve();
+        });
+      }
     });
   }
 
   static loadUser() {
     return new Promise((resolve, reject) => {
-      localForage.getItem(constant.USER_DATA)
-      .then((currentUser) => {
-        if (currentUser) {
-          resolve(currentUser);
-        } else {
-          const cookies = Cookies.getJSON(constant.USER_DATA);
-          cookies ? resolve(cookies) : reject('User not found');
-        }
-      })
-      .catch(err => reject(err));
+      if (instance.client) {
+        const cookies = Cookies.getJSON(constant.USER_DATA);
+        cookies ? resolve(cookies) : reject('User not found');
+      } else {
+        localForage.getItem(constant.USER_DATA)
+        .then((currentUser) => {
+          if (currentUser) {
+            resolve(currentUser);
+          } else {
+            const cookies = Cookies.getJSON(constant.USER_DATA);
+            cookies ? resolve(cookies) : reject('User not found');
+          }
+        })
+        .catch(err => reject(err));
+      }
     });
   }
 
